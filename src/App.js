51e03fdad31664c940a732d64c0db3340683af02
dstrivedi <drashti1118@gmail.com>
2020-08-01 02:37:52 +0530

    initail commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './App.css';
+
+function App() {
+  return (
+    <div className={classes.App}>
+        Hello TODO App
+    </div>
+  );
+}
+
+export default App;
